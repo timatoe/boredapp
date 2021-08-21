527eda72225643081e46bcb9dbec0a76e43701d9
Anthony Wakulicz <anthony.wakulicz@prezzee.com.au>
2021-08-21 22:11:50 +1000

    Add favicon

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+import os
+from flask import send_from_directory
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico')
